d3bba3affb108423a1e5243bdaace77424a4f382
Mark Thuesen Nielsen <75121460+marktn4070@users.noreply.github.com>
2025-08-22 09:13:55 +0200

    Update Room.cs

--- DomainModels/Room.cs
+++ DomainModels/Room.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,15 +10,15 @@ namespace DomainModels
     // Room.cs
     public class Room : Common
     {
-
         public required int RoomNumber { get; set; }
 
         public bool Booked { get; set; }
 
-        public required int HotelId { get; set; }
+        public int HotelId { get; set; }
         public Hotel? Hotel { get; set; }
-        public required int TypeId { get; set; } 
-        public Type? Type { get; set; }
+        public required int RoomtypeId { get; set; }
+        public Roomtype? Roomtype { get; set; }
+
         public List<Booking> Bookings { get; set; } = new();
     }
     public class RoomGetDto
@@ -29,7 +29,7 @@ namespace DomainModels
         public bool Booked { get; set; }
 
         public required int HotelId { get; set; }
-        public required int TypeId { get; set; }
+        public required int RoomtypeId { get; set; }
 
 
         public List<Booking> Bookings { get; set; } = new();
