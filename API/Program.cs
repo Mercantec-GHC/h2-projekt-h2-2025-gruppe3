fef333eea55b796f6c8683469a5bd6df82706bd2
Mark <markdirekte@gmail.com>
2025-08-26 14:01:30 +0200

    fix cors errors for deployed website 2

--- API/Program.cs
+++ API/Program.cs
@@ -108,11 +108,8 @@ public class Program
                         .WithOrigins(
                             "http://localhost:5085",
                             "http://localhost:8052",
-							"http://localhost:7285",
-							"http://127.0.0.1:7285",
-
-							"https://localhost:7285",
-							"https://127.0.0.1:7285",
+                            "http://localhost:7285",
+                            "https://localhost:7285",
                             "https://h2.mercantec.tech"
                             "https://suitedreams.mercantec.tech/"
                         )
