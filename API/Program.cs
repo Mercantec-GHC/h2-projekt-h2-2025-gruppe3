d5061c5e407f854b758f92b57c4b9022589cb94d
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-08-26 13:42:07 +0200

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -110,6 +110,9 @@ public class Program
                             "http://localhost:8052",
 							"http://localhost:7285",
 							"http://127.0.0.1:7285",
+
+							"https://localhost:7285",
+							"https://127.0.0.1:7285",
                             "https://h2.mercantec.tech"
                         )
                         .AllowAnyMethod()
