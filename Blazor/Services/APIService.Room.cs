5723cb53b274d774c8c9537c3c7544d556d767c7
Mikkel77 <Mikkelskovhansen70@gmail.com>
2025-09-03 14:50:52 +0200

    (WIP) Display rooms on Home page and add GetRoomsAsync service
    Updated Home.razor to fetch and display rooms for each hotel. Added APIService.Room.cs with GetRoomsAsync method to retrieve room data from the API.

---
+++ Blazor/Services/APIService.Room.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using DomainModels;
+
+namespace Blazor.Services
+{
+	public partial class APIService
+	{
+		public async Task<RoomGetDto[]> GetRoomsAsync(
+
+   CancellationToken cancellationToken = default
+   )
+		{
+			List<RoomGetDto>? rooms = null;
+
+			await foreach (
+				var room in httpClient.GetFromJsonAsAsyncEnumerable<RoomGetDto>(
+					"/api/rooms",
+					cancellationToken
+				)
+			)
+			{
+				if (room is not null)
+				{
+					rooms ??= [];
+					rooms.Add(room);
+				}
+			}
+
+			return rooms?.ToArray() ?? [];
+		}
+	}
+}
