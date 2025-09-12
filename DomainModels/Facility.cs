a45652678ce0f2b981a1ce13c16cc9cdbe003081
Mark <markdirekte@gmail.com>
2025-09-13 00:06:30 +0200

    merge error

--- DomainModels/Facility.cs
+++ DomainModels/Facility.cs
@@ -1,31 +1,30 @@
 using System.ComponentModel.DataAnnotations;
 namespace DomainModels;
 
-namespace DomainModels
+// Facility.cs
+public class Facility : Common
 {
-    public class Facility : Common
-    {
-        public bool Pool { get; set; }
-        public bool Fitness { get; set; }
-        public bool Restaturant { get; set; }
-        public int HotelId { get; set; }
-        public virtual Hotel? Hotel { get; set; }
-    }
-    public class FacilityGetDto
-    {
-        public int Id { get; set; }
-        public bool Pool { get; set; }
-        public bool Fitness { get; set; }
-        public bool Restaturant { get; set; }
-        public int HotelId { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
-    public class FacilityPostDto
-    {
-        public bool Pool { get; set; }
-        public bool Fitness { get; set; }
-        public bool Restaturant { get; set; }
+    public bool Pool { get; set; }
+    public bool Fitness { get; set; }
+    public bool Restaturant { get; set; }
+    public int HotelId { get; set; }
+    public Hotel? Hotel { get; set; }
+}
+public class FacilityGetDto
+{
+    public int Id { get; set; }
+    public bool Pool { get; set; }
+    public bool Fitness { get; set; }
+    public bool Restaturant { get; set; }
+    public int HotelId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
+public class FacilityPostDto
+{
+    public bool Pool { get; set; }
+    public bool Fitness { get; set; }
+    public bool Restaturant { get; set; }
 
     [Required(ErrorMessage = "Hotel ID is required")]
     public int HotelId { get; set; }
