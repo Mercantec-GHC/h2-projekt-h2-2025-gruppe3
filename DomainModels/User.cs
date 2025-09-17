2fb09b87bb703f55f126a20c96a924dd104372f7
Mikkel77 <Mikkelskovhansen70@gmail.com>
2025-09-17 11:10:23 +0200

    Update register form to use FirstName and LastName
    Changed LoginForm.razor and RegisterDto to use 'FirstName' and 'LastName' properties instead of 'Firstname' and 'Lastname' for consistency. Also uncommented and updated the relevant form fields in the registration UI.

--- DomainModels/User.cs
+++ DomainModels/User.cs
@@ -38,7 +38,6 @@ public class RegisterDto
     [Required(ErrorMessage = "Email is required")]
     public string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
-
     public string LastName { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
